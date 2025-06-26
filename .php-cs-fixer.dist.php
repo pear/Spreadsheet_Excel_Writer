4fa6097a9c286c31e63cfe93ec4a5e73ef27d9d0
Alexey Kopytko <alexey@kopytko.com>
2025-06-26 22:19:22 +0900

    .php-cs-fixer.dist.php

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,13 +1,12 @@
 <?php
 
-$finder = PhpCsFixer\Finder::create()
-    ->in(__DIR__)
-    ->exclude('vendor')
-;
-
 return (new PhpCsFixer\Config())
     ->setRules([
         'native_function_invocation' => false,
     ])
-    ->setFinder($finder)
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+            ->in(__DIR__)
+            ->append([__FILE__])
+    )
 ;
