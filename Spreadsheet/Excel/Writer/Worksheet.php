ae9fe9e0f2a08cd6707bd67e5480bece260ead0e
Alexey Kopytko <alexey@kopytko.com>
2025-06-28 18:18:38 +0900

    Update Spreadsheet/Excel/Writer/Worksheet.php

--- Spreadsheet/Excel/Writer/Worksheet.php
+++ Spreadsheet/Excel/Writer/Worksheet.php
@@ -754,7 +754,7 @@ class Spreadsheet_Excel_Writer_Worksheet extends Spreadsheet_Excel_Writer_BIFFwr
         // Return data stored in memory
         if (isset($this->_data)) {
             $tmp   = $this->_data;
-            $this->_data = null; // Set to null instead of unset for PHP 8.4 compatibility
+            $this->_data = null;
             if ($this->_using_tmpfile) {
                 fseek($this->_filehandle, 0);
             }
